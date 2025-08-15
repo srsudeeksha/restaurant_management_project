a3591ddd222cfaf4b82e3dda7f885cabedf77222
srsudeeksha <srsudeeksha@gmail.com>
2025-08-15 16:55:35 +0530

    31666

--- views.py
+++ views.py
@@ -1,15 +1,14 @@
+from django.shortcuts import render 
+from django.http import HttpResponse
 from datetime import datetime
 
 def home(request):
-    context = {
-        'restaurant_name': 'Nothing Before Milkshake',
-        'welcome_message': 'Welcome to our milkshake paradise!',
-        'current_year': datetime.now().year
-    }
-    return render(request, 'home.html', context)
-
-def reservations(request):
-    context = {
-        'current_year': datetime.now().year
-    }
-    return render(request, 'reservations.html', context)
+    try:
+        context = {
+            'restaurant_name': 'Nothing Before Milkshake',
+            'welcome_message': 'Welcome to our milkshake paradise!',
+            'current_year': datetime.now().year
+        }
+        return render(request, 'home.html', context)
+    except Exception as e:
+        return HttpResponse(f"An error occured: {str(e)}", status = 500)   
