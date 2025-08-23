c597d75d45627736e8ccc3c8fe4d8da4b22f5955
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-08-23 16:53:01 +0530

    37354

--- admin.py
+++ admin.py
@@ -1,13 +1,6 @@
-from django.contrib import admin
-from .models import Menu, Order
+from django.contrib import admin 
+from .models import Menu, Order, OrderItem
 
-@admin.register(Menu)
-class MenuAdmin(admin.ModelAdmin):
-    list_display = ("name","price","description")
-    search_fields = ("name",)
-
-@admin.register(Order)
-class OrderAdmin(admin.ModelAdmin):
-    list_display=("id","Customer_name","menu_item","quantity","created_at")
-    list_filter=("created_at",)
-    search_fields=("customer_name",)
+admin.site.register(Menu)
+admin.site.register(Order)
+admin.site.register(OrderItem)
