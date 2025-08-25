552f9c4a5e0af263fdf2d28776725e6756522cfa
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-08-25 22:02:07 +0530

    38356

--- admin.py
+++ admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-from .models import Userprofile
+from .models import MenuItem
 
-@admin.register(Userprofile)
-class UserprofileAdmin(admin.ModelAdmin):
-    list_display = ("user","phone_number")
+admin.site.register(MenuItem)
