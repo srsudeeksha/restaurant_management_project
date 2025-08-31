426d336f5d480588f13088f97a4f4b05508b9c66
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-08-31 16:32:59 +0530

    Update views.py

--- home/views.py
+++ home/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+def home(request):
+    menu_items = [
+        {"name": "Margherita Pizza", "price": 250},
+        {"name": "Veg Burger", "price": 150},
+        {"name": "French Fries", "price": 100},
+        {"name": "Chocolate Milkshake", "price": 180},
+        {"name": "Pasta Alfredo", "price": 220},
+    ]
 
-# Create your views here.
+    query = request.GET.get("q")
+    if query:
+        menu_items = [item for item in menu_items if query.lower() in item["name"].lower()]
+
+    return render(request, "home.html", {"menu_items": menu_items, "query": query})
