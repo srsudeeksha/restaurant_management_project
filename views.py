252747faa0151cea12622b3e75066be55cf9953c
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-08-31 17:54:59 +0530

    41146

--- views.py
+++ views.py
@@ -1,14 +1,6 @@
+from django.shortcuts import render
+from .models import Restaurant
+
 def home(request):
-    menu_items=[
-        {"name":"Margherita Pizza","price":250},
-        {"name":"Veg Burger","price":150},
-        {"name":"French Fries","price":100},
-        {"name":"Chocolate Milkshake","price":180},
-        {"name":"Pasta Alfredo","price":220},
-    ]
-
-    query = request.GET.get("q")
-    if query:
-        menu_item = [item for item in menu_item if query.lower()in item["name"].lower()]
-
-    return render(request,"home.html",{"menu_items":menu_items,"query":query})
+    restaurant = Restaurant.objects.first()
+    return render(request,"home.html",{"restaurant":restaurant})
