ee2e066590474ee2f7b06a4f710fdd740fe591da
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-01 12:41:44 +0530

    Update models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=15, default="")
+
+    def __str__(self):
+        return self.name
