4191d8c19d79792eebe5266cb37fdae86489ac44
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-09 22:35:54 +0530

    43740

--- urls.py
+++ urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
-from . import views
+from .views import homepage
 
 urlpatterns = [
-    path("menu/", views.menu_list, name="menu")
-    path("add-to-cart/<int:item_id>/", views.add_to_cart, name="add_to_cart")
-    path("cart/", views.view_cart, name="view_cart"),
+    path('',homepage, name='home'),
 ]
