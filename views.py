9b29c2563c7c78ea640d70cda5a2053a3350f3eb
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-11 15:46:49 +0530

    43973

--- views.py
+++ views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Restaurant
-def home(request):
-    restaurant = Restaurant.objects.first()
-    return render(request,'home.html',{'restaurant',restaurant})
+from django.views.generic import ListView
+from .models import MenuItem
+
+class MenuListView(ListView):
+    model = MenuItem
+    template_name = 'menu_list.html'
+    context_object_name = 'menu_items'
+    paginate_by = 10 
