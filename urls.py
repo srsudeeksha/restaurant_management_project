5d08e4989e13d9598c7a2208405b76538af5cb93
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-13 15:38:14 +0530

    43970

--- urls.py
+++ urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("contact/",views.contact_view, name="contact"),
-    path("thank_you/",views.thank_you_view, name="thank_you"),
-
+    path("menu/",views.name name="memu"),
 ]
