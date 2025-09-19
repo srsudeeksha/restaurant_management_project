2e41ff40cd4c4f69eeaa1acb2a92beeaa112b869
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-19 11:27:10 +0530

    49272

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class ActiveOrderManager(models.Manager):
+    def get_active_orders(self):
+        return self.gat_queryset().filter(status__in=['pending','processing'])
+
+class Order(models.Model):
+    status = models.CharField(max_length=20)
+
+    objects= ActiveOrderManager()
