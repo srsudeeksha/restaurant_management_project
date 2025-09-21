ae766a4204c03f5f109edd67dd973a01cd7afb00
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-21 23:38:51 +0530

    49984

--- orders/models.py
+++ orders/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+from decimal import Decimal 
 
-class ActiveOrderManager(models.Manager):
-    def get_active_orders(self):
-        return self.gat_queryset().filter(status__in=['pending','processing'])
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
 
 class Order(models.Model):
-    status = models.CharField(max_length=20)
 
-    objects= ActiveOrderManager()
+    def calculate_total(self):
+        return sum(
+            item.menu_item.price * item.quantity
+            for item in self.orderitem_set.all()
+
+        )
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    
