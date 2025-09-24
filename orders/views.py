47c4a8d589e5dbf0eba2254a70172faeca878920
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-24 17:17:53 +0530

    50814

--- orders/views.py
+++ orders/views.py
@@ -3,15 +3,20 @@ from rest_framework.response import Response
 from rest_framework import status as drf_status
 from .models import Order
 
-class CancelOrderAPIView(APIView):
-    def delete(self, request, order_id):
+class OrderStatusUpdateView(APIView):
+    def post(self, request):
+        order_id = request.data.get('order_id')
+        new_status = request.data.get('status')
+
         try:
-            order = Order.objects.get(id=order_id)
-            if order.customer != request.user:
-                return Response({'error':'You can only cancel your own orders.'}, status = drf_status.HTTP_403_FORBIDDEN)
-            order.status = 'Cancelled'
-            order.save()
-            return Response({'status':'Order cancelled.'}, status=drf_status.HTTP_200_OK)
+            order = Order.Objects.get(id=order_id)
         except Order.DoesNotExist:
             return Response({'error':'Order not found.'}, status=drf_status.HTTP_404_NOT_FOUND)
-            
+
+        allowed_statuses = dict(Order.STATUS_CHOICES).keys()
+        if new_statuses not in allowed_statuses:
+            return Response({'error':'Invalid status'}, status=drf_status.HTTP_400_BAD_REQUEST)
+
+        order.status = new_status
+        order.save()
+        return Response({'message':'Status updated.','order_id':order_id, 'new_status':new_status})
