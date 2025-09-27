a9a09ea02335a049999301cb69e031a2849fe94d
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-09-27 17:25:51 +0530

    51264

--- views.py
+++ views.py
@@ -1,7 +1,13 @@
-from rest_framework import viewsets
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from rest_framework import generics 
+from .models import UserReview
+from .serializers import UserReviewSerializer
 
-class MenuCategoryViewSet(viewsets.ModelViewSet):
-    queryset = MenuCategory.objects.all()
-    serializer_class = MenuCategorySerializer
+class UserReviewCreateView(generics.CreateAPIView):
+    serializer_class = UserReviewSerializer
+
+class MenuItemReviewListView(generics.ListAPIView):
+    serializer_class=UserReviewSerializer
+
+    def get_queryset(self):
+        menu_item_id = self.request.query_params.get('menu_item')
+        return UserReview.objects.filter(menu_item__id=menu_item_id)
