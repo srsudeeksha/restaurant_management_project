2ecc6da555c7d4f9249595b5dfbe2bfe6929e11c
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-10-02 21:09:49 +0530

    53008

--- views.py
+++ views.py
@@ -1,12 +1,7 @@
-from rest_framework.viewss import APIView
-from rest_framework.response import Response
-from .models import Restaurant
-from .serializers import RestaurantSerializer
+from rest_framework import generics
+from .models import Table
+from .serializers import TableSerializers
 
-class RestaurantInfoView(APIView):
-    def get(self, request):
-        restaurant = Restaurant.objects.first()
-        if restaurant is None:
-            return Response({'error':'No restaurant found.'}, status = 404)
-        serializer = RestaurantSerializer(restaurant)
-        return Response(serializer.data)
+class TableSerializersAPIView(generics.RetrieveAPIView):
+    queryset = Table.objects.all()
+    serializer_class = TableSerializer
