2b75158bfe1f79ca5226dcd111a6e93cf57758b3
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-10-02 21:12:43 +0530

    53007

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
-import string 
-import secrets
-from .models import Order
+from datetime import datetime, time
 
-def generate_unique_order_id(length=8):
-    chars = string.ascii_uppercase + string.digits
-    while True:
-        candidate = ''.join(secrets.choice(chars)for _ in range(length))
-        if not Order.objects.filter(order_id=candidate).exists():
-            return candidate
+def is_restaurant_open():
+    now = datetime.now()
+    weekday = now.weekday()
+
+    open_time = time(9,0)
+    close_time = time(22,0)
+
+    current_time = now.time()
+    return open_time <= current_time <= close_time
