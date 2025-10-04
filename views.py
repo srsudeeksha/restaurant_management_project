f190f026a4eac1e7eaaed888ee415cf33b09315f
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-10-04 21:17:18 +0530

    53351

--- views.py
+++ views.py
@@ -1,8 +1,13 @@
-from rest_framework import generics
-from .models import MenuCategory
-from .serializers import MenuCategorySerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ReviewSerializer
 
-class MenuCategoryListView(generics.ListAPIView):
-    querset = MenuCategory.objects.all()
-    serializer_class=MenuCategorySerializer
-    
+class ReviewCreateView(APIView):
+    def post(self, request):
+        serializer = ReviewSerializer(data = request.data)
+        if serializer.is_valid():
+            serialier.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
