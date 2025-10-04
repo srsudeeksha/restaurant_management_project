be058ef4d0a1125d460ea9c53384847a0d02666a
Sannidhi Ranga Sudeeksha <srsudeeksha@gmail.com>
2025-10-04 21:05:56 +0530

    53352

--- models.py
+++ models.py
@@ -1,13 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
-from .models import MenuItem
 
-class UserReview(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    rating =  models.IntegerField()
-    comment = models.TextField()
-    review_date = models.DateTimeField(auto_now_add=True)
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+    discount_percentage = models.FloatField(default=0)
 
-    def __str__(self):
-        return f"{self.user.username} review for {self.menu_item.name}:{self.rating}
+    def get_final_price(self):
+        """
+        Return the final price after applying discount.
+        """
+        discount = (self.price * self.discount_percentage)/100
+        return round(self.price-discount,2)
+        
